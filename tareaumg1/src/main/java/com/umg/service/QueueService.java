2060ae9abb35a495049bfaaf67e169cc9b88fdbb
ng-arch <57119686+ng-arch@users.noreply.github.com>
2021-02-18 13:19:42 -0600

    Add files via upload
    ADD
    - first version

---
+++ tareaumg1/src/main/java/com/umg/service/QueueService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.umg.service;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Nestor Gaitan
+ */
+public class QueueService {
+ 
+    private Vector vector=new Vector(20);
+
+    public QueueService() {
+        
+    }
+    
+    
+
+    public Vector getVector() {
+        return vector;
+    }
+
+    public void setVector(Vector vector) {
+        this.vector = vector;
+    }
+    
+    
+    
+}
